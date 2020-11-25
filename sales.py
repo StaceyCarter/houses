386f7da98828775c84da9281d4f6d62c3154eff1
Stacey Carter <stak1903@gmail.com>
2020-11-25 13:26:30 -0800

    add sales test

--- sales.py
+++ sales.py
@@ -1,38 +1,52 @@
 from model import SalesModel
 import pprint
+from datetime import datetime
 
 
 class Sales:
-    def __init__(self, sales_dict):
-        self.sales_model = self.create_entry(sales_dict)
+    def __init__(self, sales_dict, auction_date):
+        sales_data = self.create_data(sales_dict, auction_date)
+        self.sales_model = self.create_entry(**sales_data)
 
-    def create_entry(self, sales_dict):
-        sales_model = SalesModel(
-            auctionDate=sales_dict.get("auctionDate"),
-            suburb=sales_dict.get("suburb"),
-            street_number=sales_dict.get("streetNumber"),
-            street_name=sales_dict.get("streetName"),
-            street_type=sales_dict.get("streetType"),
-            postcode=sales_dict.get("postcode"),
-            state=sales_dict.get("state"),
-            property_type=sales_dict.get("propertyType"),
-            bedrooms=sales_dict.get("bedrooms"),
-            car_spaces=sales_dict.get("carspaces"),
-            bathrooms=sales_dict.get("bathrooms"),
-            property_details_url=sales_dict.get("domainPropertyDetailsUrl"),
-            agent=sales_dict.get("agent"),
-            agency_name=sales_dict.get("agencyName"),
-            geom=self.get_geometry_string(sales_dict.get("geoLocation")),
-        )
+    def create_entry(self, **kwargs):
+        sales_model = SalesModel(**kwargs)
         return sales_model
+
+    def create_data(self, sales_dict, auction_date):
+        sales_data = {
+            "auctionDate": self.get_date(auction_date),
+            "suburb": sales_dict.get("suburb"),
+            "street_number": sales_dict.get("streetNumber"),
+            "street_name": sales_dict.get("streetName"),
+            "street_type": sales_dict.get("streetType"),
+            "postcode": sales_dict.get("postcode"),
+            "state": sales_dict.get("state"),
+            "property_type": sales_dict.get("propertyType"),
+            "bedrooms": sales_dict.get("bedrooms"),
+            "car_spaces": sales_dict.get("carspaces"),
+            "bathrooms": sales_dict.get("bathrooms"),
+            "property_details_url": sales_dict.get("domainPropertyDetailsUrl"),
+            "agent": sales_dict.get("agent"),
+            "agency_name": sales_dict.get("agencyName"),
+            "geom": self.get_geometry_string(sales_dict.get("geoLocation")),
+        }
+        self.sales_data = sales_data
+        return sales_data
 
     def get_geometry_string(self, lat_long_dict):
         lat = lat_long_dict.get("latitude")
         lon = lat_long_dict.get("longitude")
         return f"SRID=4326;POINT({lat} {lon})"
 
+    def get_date(self, date_time_string):
+        date_string = date_time_string.split("T")[
+            0
+        ]  # Get date only w/o timezone and time info
+        date_format = "%Y-%m-%d"
+        return datetime.strptime(date_string, date_format)
+
     def get_sales_model(self):
         return self.sales_model
 
     def __str__(self):
-        return f"{pprint.pformat(vars(self.get_sales_model()))}"
+        return f"{pprint.pformat(self.sales_data)}"
--- tests/test_sales.py
+++ tests/test_sales.py
@@ -9,47 +9,76 @@ if __name__ == "__main__" and __package__ is None:
 from unittest import TestCase
 import unittest
 import json
+import datetime
 from sales import Sales
+from model import SalesModel
+from main import extract_data
 
 from pathlib import Path
 
 
 def get_test_listing():
     return {
-        "streetNumber": "4",
-        "streetName": "Alma",
-        "streetType": "St",
-        "suburb": "Aberfeldie",
-        "postcode": "3040",
-        "state": "Vic",
-        "geoLocation": {"latitude": -37.757401, "longitude": 144.9034335},
-        "propertyType": "House",
-        "bedrooms": 4,
-        "bathrooms": 2,
-        "carspaces": 4,
-        "price": 1700000,
-        "result": "AUVB",
-        "agent": "Nelson Alexander Essendon",
-        "domainId": "2016543328",
-        "domainAgencyId": "4722",
-        "agencyName": "Nelson Alexander Essendon",
-        "agencyProfilePageUrl": "http://www.domain.com.au/real-estate-agencies/nelsonalexanderessendon-4722",
-        "domainPropertyDetailsUrl": "https://www.domain.com.au/4-alma-street-aberfeldie-vic-3040-2016543328",
+        "test_listing": {
+            "streetNumber": "4",
+            "streetName": "Alma",
+            "streetType": "St",
+            "suburb": "Aberfeldie",
+            "postcode": "3040",
+            "state": "Vic",
+            "geoLocation": {"latitude": -37.757401, "longitude": 144.9034335},
+            "propertyType": "House",
+            "bedrooms": 4,
+            "bathrooms": 2,
+            "carspaces": 4,
+            "price": 1700000,
+            "result": "AUVB",
+            "agent": "Nelson Alexander Essendon",
+            "domainId": "2016543328",
+            "domainAgencyId": "4722",
+            "agencyName": "Nelson Alexander Essendon",
+            "agencyProfilePageUrl": "http://www.domain.com.au/real-estate-agencies/nelsonalexanderessendon-4722",
+            "domainPropertyDetailsUrl": "https://www.domain.com.au/4-alma-street-aberfeldie-vic-3040-2016543328",
+        },
+        "expected_result": {
+            "agency_name": "Nelson Alexander Essendon",
+            "agent": "Nelson Alexander Essendon",
+            "auctionDate": datetime.datetime(2020, 11, 21, 0, 0),
+            "bathrooms": 2,
+            "bedrooms": 4,
+            "car_spaces": 4,
+            "geom": "SRID=4326;POINT(-37.757401 144.9034335)",
+            "postcode": "3040",
+            "property_details_url": "https://www.domain.com.au/4-alma-street-aberfeldie-vic-3040-2016543328",
+            "property_type": "House",
+            "state": "Vic",
+            "street_name": "Alma",
+            "street_number": "4",
+            "street_type": "St",
+            "suburb": "Aberfeldie",
+        },
     }
 
 
 class TestSales(TestCase):
+    def test_create_sales_entry(self):
+        listing = get_test_listing()
+        test_listing = listing["test_listing"]
+        expected_sales_data = listing["expected_result"]
+        entry = Sales(test_listing, "2020-11-21T00:00:00")
+        self.assertIsInstance(entry.get_sales_model(), SalesModel)
+        self.assertDictEqual(entry.sales_data, expected_sales_data)
+
+
+class TestExtractData(TestCase):
     def setUp(self):
         path = Path(__file__).parent / "./test_data.json"
         with path.open() as f:
             d = json.load(f)
             self.test_data = d
 
-    def test_create(self):
-        print("\n\n here")
-
-        sales_data = Sales(get_test_listing())
-        print("print S: ", sales_data)
+    def test_extract_data(self):
+        pass
 
 
 if __name__ == "__main__":
