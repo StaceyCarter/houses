68efacebb7a0afd3d316f6f61d205ddd4a79dda3
Stacey Carter <stak1903@gmail.com>
2020-11-24 20:45:59 -0800

    add basic model creation + string representation

--- sales.py
+++ sales.py
@@ -1,3 +1,36 @@
+from model import SalesModel
+import pprint
+
+
 class Sales:
-    def __init__(self, **kwargs):
-        self.sales_data = {**kwargs}
+    def __init__(self, sales_dict):
+        self.sales_model = self.create_entry(sales_dict)
+
+    def create_entry(self, sales_dict):
+        sales_model = SalesModel(
+            auctionDate=sales_dict.get("auctionDate"),
+            suburb=sales_dict.get("suburb"),
+            street_number=sales_dict.get("streetNumber"),
+            street_name=sales_dict.get("streetName"),
+            street_type=sales_dict.get("streetType"),
+            postcode=sales_dict.get("postcode"),
+            state=sales_dict.get("state"),
+            property_type=sales_dict.get("propertyType"),
+            bedrooms=sales_dict.get("bedrooms"),
+            car_spaces=sales_dict.get("carspaces"),
+            bathrooms=sales_dict.get("bathrooms"),
+            property_details_url=sales_dict.get("domainPropertyDetailsUrl"),
+            agent=sales_dict.get("agent"),
+            agency_name=sales_dict.get("agencyName"),
+            geom=self.get_geometry_string(sales_dict.get("geoLocation")),
+        )
+        return sales_model
+
+    def get_geometry_string(self, lat_long_dict):
+        pass
+
+    def get_sales_model(self):
+        return self.sales_model
+
+    def __str__(self):
+        return f"{pprint.pformat(vars(self.get_sales_model()))}"
--- tests/test_sales.py
+++ tests/test_sales.py
@@ -1,12 +1,44 @@
+if __name__ == "__main__" and __package__ is None:
+    from sys import path
+    from os.path import dirname as dir
+
+    path.append(dir(path[0]))
+
+    __package__ = "houses"
+
 from unittest import TestCase
 import unittest
 import json
-from ..sales import Sales
+from sales import Sales
 
 from pathlib import Path
 
 
-class Test(TestCase):
+def get_test_listing():
+    return {
+        "streetNumber": "4",
+        "streetName": "Alma",
+        "streetType": "St",
+        "suburb": "Aberfeldie",
+        "postcode": "3040",
+        "state": "Vic",
+        "geoLocation": {"latitude": -37.757401, "longitude": 144.9034335},
+        "propertyType": "House",
+        "bedrooms": 4,
+        "bathrooms": 2,
+        "carspaces": 4,
+        "price": 1700000,
+        "result": "AUVB",
+        "agent": "Nelson Alexander Essendon",
+        "domainId": "2016543328",
+        "domainAgencyId": "4722",
+        "agencyName": "Nelson Alexander Essendon",
+        "agencyProfilePageUrl": "http://www.domain.com.au/real-estate-agencies/nelsonalexanderessendon-4722",
+        "domainPropertyDetailsUrl": "https://www.domain.com.au/4-alma-street-aberfeldie-vic-3040-2016543328",
+    }
+
+
+class TestSales(TestCase):
     def setUp(self):
         path = Path(__file__).parent / "./test_data.json"
         with path.open() as f:
@@ -14,10 +46,10 @@ class Test(TestCase):
             self.test_data = d
 
     def test_create(self):
-        import pprint
+        print("\n\n here")
 
-        pprint.pprint(self.test_data)
-        print("here")
+        sales_data = Sales(get_test_listing())
+        print("print S: ", sales_data)
 
 
 if __name__ == "__main__":
